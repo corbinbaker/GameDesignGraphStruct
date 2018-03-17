f68177d56035a8d24ff7d53b95ee57714eb674c7
corbinbaker <corbinbaker@mymail.mines.edu>
2018-03-16 19:12:36 -0600

    corrected header files, created Vertex struct containing its name and a vector of other vertices and their costs

--- GameDesignGraph/driver.h
+++ GameDesignGraph/driver.h
@@ -2,49 +2,33 @@
 
 
 //TEST DRIVER
-struct TestDriver {
+
 	
 	//INIT – Reset the graph
-	void INIT() {
-
-	}
+	void INIT();
 
 
 	//ADDV – Request a string, then add that string as a vertex.
-	void ADDV() {
-
-	}
+	void ADDV();
 	
 	
 	//ADDE – Request the name of two vertices, then a weight between the vertices.Creates an edge based on this info.
-	void ADDE() {
-
-	}
+	void ADDE();
 
 
 	//PRINT – Prints each vertex in the graph, along with all edges and weights associated with the vertex.
 	//The output might look like…
 	//Atlanta; Adjacent: Houston (800), Washington (600)
-	void PRINT() {
-
-	}
+	void PRINT();
 
 
 	//TEST1 – Initializes the graph.Then insert the vertices and edges defined for the example on slide 6 in lecture 24 (Game Map and Graphs).
-	void TEST1() {
-
-	}
+	void TEST1();
 
 
 	//TEST2 (Bonus Points) - Initializes the graph. Then insert the vertices and edges defined for the example on slide 3 (Movealot) in lecture 24.
-	void TEST2() {
-
-	}
-
+	void TEST2();
 
 
 	//QUIT – Quit the test program.
-	void QUIT() {
-
-	}
-};
+	void QUIT();
--- GameDesignGraph/graph.h
+++ GameDesignGraph/graph.h
@@ -3,37 +3,32 @@
 #include <queue>
 using namespace std;
 
-//GRAPH - strings for the vertices and ints for the weights
-struct Graph {
-
-	vector<string> vertexes;
-	
-	//void makeEmpty() – Initializes the graph to an empty state.
-	void makeEmpty() {
-
-	}
-
-
-	//void addVertex(string vertexname) – Add a vertex to the graph.
-	void addVertex(string vertexname) {
-
-	}
-
-
-	//bool addEdge(string fromVertex, string toVertex, int cost) - Adds an edge from fromVertex to toVertex with a weight of cost.  Returns false if either vertex not found.
-	bool addEdge(string fromVertex, string toVertex, int cost) {
-
-	}
-
-
-	//int getWeight( string fromVertex, string toVertex ) – Returns the cost to move from fromVertex to adjacent vertex toVertex. If the two vertices are not adjacent or not found, returns -1.
-	int getWeight(string fromVertex, string toVertex) {
-
-	}
-
-	//bool getAdjacent( string fromVertex, queue vertexQue) – given the vertex fromVertex, return a queue containing the adjacent vertices.  Returns false if vertex not found.
-	bool getAdjacent(string fromVertex, queue<string> vertexQue) {
-
-	}
+struct Vertex {
+	string name;
+	vector<Vertex, int> costs;
 };
+
+
+//GRAPH - strings for the vertices and ints for the weights
+vector<Vertex> vertices;
+
+//void makeEmpty() – Initializes the graph to an empty state.
+void makeEmpty();
+
+
+//void addVertex(string vertexname) – Add a vertex to the graph.
+void addVertex(string vertexname);
+
+
+//bool addEdge(string fromVertex, string toVertex, int cost) - Adds an edge from fromVertex to toVertex with a weight of cost.  Returns false if either vertex not found.
+bool addEdge(string fromVertex, string toVertex, int cost);
+
+
+//int getWeight( string fromVertex, string toVertex ) – Returns the cost to move from fromVertex to adjacent vertex toVertex. If the two vertices are not adjacent or not found, returns -1.
+int getWeight(string fromVertex, string toVertex);
+
+
+//bool getAdjacent( string fromVertex, queue vertexQue) – given the vertex fromVertex, return a queue containing the adjacent vertices.  Returns false if vertex not found.
+bool getAdjacent(string fromVertex, queue<string> vertexQue);
+
 
